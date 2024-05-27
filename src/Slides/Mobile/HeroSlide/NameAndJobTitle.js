12c9d796f2f5b2916edc6ef57587ea5758ab3f4c
Thallison <thallisonrlima@gmail.com>
2024-05-27 12:57:19 -0300

    Remove vscode settings and change home name

--- src/Slides/Mobile/HeroSlide/NameAndJobTitle.js
+++ src/Slides/Mobile/HeroSlide/NameAndJobTitle.js
@@ -59,8 +59,8 @@ class NameAndJobTitle extends Component {
   render() {
     return (
       <Container>
-        <Name>Suresh Murali</Name>
-        <Title>UI/UX DESIGNER & DEVELOPER</Title>
+        <Name>Thallison Rhanniel</Name>
+        <Title>UI/UX DESIGNER & MOBILE DEVELOPER</Title>
       </Container>
     );
   }
--- src/Slides/WideScreen/HeroSlide/NameAndJobTitle.js
+++ src/Slides/WideScreen/HeroSlide/NameAndJobTitle.js
@@ -18,9 +18,9 @@ class NameAndJobTitle extends Component {
   render() {
     return (
       <Container>
-        <NameReveal text="Suresh Murali" fontFam="Valencia" timeDelay={500} />
+        <NameReveal text="Thallison Rhanniel" fontFam="Valencia" timeDelay={500} />
         <br />
-        <TitleReveal text="UI/UX Designer & Developer" fontFam="AvenirRoman" timeDelay={1300} />
+        <TitleReveal text="U/UX Designer & Mobile Developer" fontFam="AvenirRoman" timeDelay={1300} />
       </Container>
     );
   }
